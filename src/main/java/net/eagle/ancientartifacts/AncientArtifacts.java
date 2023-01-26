8b42093daf7b7e18ab01c32088e57d1d496c08f0
Brandon Poletti <brandon.poletti@gmail.com>
2023-01-26 18:37:27 -0500

    first commit

---
+++ src/main/java/net/eagle/ancientartifacts/AncientArtifacts.java
@@ -0,0 +1,17 @@
+package net.eagle.ancientartifacts;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AncientArtifacts implements ModInitializer {
+
+	public static final String ANCIENTARTIFACTS = "ancientartifacts";
+	public static final Logger LOGGER = LoggerFactory.getLogger(ANCIENTARTIFACTS);
+
+	@Override
+	public void onInitialize() {
+		
+	}
+}
---
+++ src/main/java/net/eagle/ancientartifacts/AncientArtifactsClient.java
@@ -0,0 +1,13 @@
+package net.eagle.ancientartifacts;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AncientArtifactsClient implements ClientModInitializer{
+
+    @Override
+    public void onInitializeClient() {
+
+        
+    }
+    
+}
