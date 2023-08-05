950afda982d695b2f8ce415b65586dbc3bac5294
Brandon Poletti <brandon.poletti@gmail.com>
2023-08-05 17:01:44 -0400

    made mod actually playable in survival

--- src/main/java/net/eagle/ancientartifacts/block/ModBlocks.java
+++ src/main/java/net/eagle/ancientartifacts/block/ModBlocks.java
@@ -16,27 +16,27 @@ import net.minecraft.util.registry.Registry;
 public class ModBlocks {
     
     public static final Block NENDER_BRICK = registerBlock("nender_brick", 
-    new Block(FabricBlockSettings.of(Material.STONE).strength(4f).requiresTool()), ModItemGroup.ARTIFACTS);
+    new Block(FabricBlockSettings.of(Material.STONE).strength(2f).requiresTool()), ModItemGroup.ARTIFACTS);
 
     public static final Block CHACHAPOYAN_IDOL = registerBlock("chachapoyan_idol",
-    new ChachapoyanIdol(FabricBlockSettings.of(Material.METAL).luminance(8).strength(4f).
+    new ChachapoyanIdol(FabricBlockSettings.of(Material.METAL).luminance(8).strength(3f).
             requiresTool().nonOpaque()), ModItemGroup.ARTIFACTS);
 
     public static final Block GILDED_PLATE = registerBlock("gilded_plate",
-            new GildedPlate(FabricBlockSettings.of(Material.STONE).strength(4f).requiresTool()), ModItemGroup.ARTIFACTS);
+            new GildedPlate(FabricBlockSettings.of(Material.STONE).strength(1f).requiresTool()), ModItemGroup.ARTIFACTS);
 
     public static final Block DRAGON_PEDESTAL = registerBlock("dragon_pedestal",
-            new DragonPedestal(FabricBlockSettings.of(Material.STONE).strength(4f).requiresTool()), ModItemGroup.ARTIFACTS);
+            new DragonPedestal(FabricBlockSettings.of(Material.STONE).strength(2f).requiresTool()), ModItemGroup.ARTIFACTS);
 
     public static final Block ETHER_LEVER = registerBlock("ether_lever",
-            new EtherLever(FabricBlockSettings.of(Material.STONE).strength(4f).requiresTool()), ModItemGroup.ARTIFACTS);
+            new EtherLever(FabricBlockSettings.of(Material.STONE).strength(1f).requiresTool()), ModItemGroup.ARTIFACTS);
     public static final Block TOTEM_OF_ORDER = registerBlock("totem_of_order",
-            new TotemOrder(FabricBlockSettings.of(Material.METAL).strength(4f).requiresTool()), ModItemGroup.ARTIFACTS);
+            new TotemOrder(FabricBlockSettings.of(Material.METAL).strength(1f).requiresTool()), ModItemGroup.ARTIFACTS);
     public static final Block TOTEM_OF_CHAOS = registerBlock("totem_of_chaos",
-            new TotemChaos(FabricBlockSettings.of(Material.METAL).strength(4f).requiresTool()), ModItemGroup.ARTIFACTS);
+            new TotemChaos(FabricBlockSettings.of(Material.METAL).strength(1f).requiresTool()), ModItemGroup.ARTIFACTS);
 
     public static final Block COPPER_WIRE = registerBlock("copper_wire",
-            new CopperWire(FabricBlockSettings.of(Material.METAL).strength(4f).requiresTool()), ModItemGroup.ARTIFACTS);
+            new CopperWire(FabricBlockSettings.of(Material.METAL).strength(1f).requiresTool()), ModItemGroup.ARTIFACTS);
 
     private static Block registerBlock(String name, Block block, ItemGroup tab) {
         registerBlockItem(name, block, tab);
